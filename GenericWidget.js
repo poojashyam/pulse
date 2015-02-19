ea06fdedc94532266264a8b67d46cd6123e84c08
KaranTabrizi <Karan.tabrizi@photoninfotech.net>
2015-02-19 11:46:20 -0600

    lets see what happens now

--- GenericWidget.js
+++ GenericWidget.js
@@ -10,5 +10,8 @@ Clazz.GenericWidget = Clazz.extend(
         var widgeta=new Clazz.footerWidget(this.config.data);
         var widgetacontainer = document.getElementById("footercontainer");
         widgeta.render(widgetacontainer);
+        var headerWidget=new Clazz.headerWidget(this.config);
+        var headerContainer = document.getElementById("headerContainer");
+        headerWidget.render(headerContainer);
         }
 });
---
+++ headerWidget.js
@@ -0,0 +1,8 @@
+Clazz.headerWidget = Clazz.extend(
+    Clazz.WidgetWithTemplate,{
+    
+        initialize : function(config){
+            this.data = config;
+            this.templateUrl = "header.html";
+        }
+});
